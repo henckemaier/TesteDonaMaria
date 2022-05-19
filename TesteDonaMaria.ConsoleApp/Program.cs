7ddbd274625dd570162b2fa5896e92193e35602d
Eduardo Henckemaier <eduhenckemaier@outlook.com>
2022-05-19 14:43:21 -0300

    .

---
+++ TesteDonaMaria.ConsoleApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using TesteDonaMaria.Dominio.ModuloMateria;
+
+namespace TesteDonaMaria.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Materia novaMateria = ObterMateria();
+
+            //abrir conexao com o banco de dados
+            string enderecoBanco = 
+                "Data Source=(LocalDB)\\MSSqlLocalDB;"+
+                "Initial Catalog=TesteDonaMariaDb;"+
+                "Integrated Security=True;"+
+                "Pooling=False";
+            SqlConnection conexaoComBanco = new SqlConnection();
+            conexaoComBanco.ConnectionString = enderecoBanco;
+            conexaoComBanco.Open();
+
+
+            //criar comando de inserção
+            SqlCommand comandoInsercao = new SqlCommand();
+            comandoInsercao.Connection = conexaoComBanco;
+            string sql = @"INSERT INTO[TBMATERIA]
+                            (
+                                [NOMEMATERIA], 
+		                        [DISCIPLINA],
+		                        [SERIE]
+	                        )
+	                        VALUES
+                            (
+                                @n,
+                                @d,
+                                @s
+                            )";
+
+            comandoInsercao.CommandText = sql;
+
+            //passar parametros para o comando inserção
+            comandoInsercao.Parameters.AddWithValue("n", novaMateria.NomeMateria);
+            comandoInsercao.Parameters.AddWithValue("d", novaMateria.Disciplina);
+            comandoInsercao.Parameters.AddWithValue("s", novaMateria.Serie);
+
+
+            //executar o comando
+            comandoInsercao.ExecuteNonQuery();
+
+            //fechar conexão
+            conexaoComBanco.Close();
+        }
+
+        private static Materia ObterMateria()
+        {
+            return new Materia
+            {
+                NomeMateria = "Algebra",
+                Disciplina = (MateriaDisciplinaEnum)0,
+                Serie = (SerieMateriaEnum)1
+            };
+        }
+    }
+}
---
+++ TesteDonaMaria.Infra.BancoDados/Scripts Iniciais.sql
@@ -0,0 +1,60 @@
+﻿--Criando a tabela de Materias
+	CREATE TABLE [dbo].[TBMateria] (
+		[Numero]      INT          IDENTITY (1, 1) NOT NULL,
+		[NomeMateria] VARCHAR (50) NOT NULL,
+		[Disciplina]  INT          NOT NULL,
+		[Serie]       INT          NOT NULL,
+		PRIMARY KEY CLUSTERED ([Numero] ASC)
+	);
+
+
+--Inserindo um registro na tabela
+	INSERT INTO [TBMATERIA] 
+	(
+		[NOMEMATERIA], 
+		[DISCIPLINA],
+		[SERIE]
+	)
+	VALUES 
+	(
+		'Flora Brasileira',
+		1,
+		2
+	); 
+	--select Scope_Identity();
+
+--Atualizando um registro da tabela
+	UPDATE [TBMATERIA]	
+		SET
+			[NOMEMATERIA] = 'Flora Americana', 
+			[DISCIPLINA] = 3,
+			[SERIE] = 2
+		WHERE
+			[NUMERO] = 2
+
+--Excluindo um registro da tabela
+	DELETE FROM [TBMATERIA]
+		WHERE 
+			[NUMERO] = 6
+
+--Retornando todos os registros da tabela
+	SELECT 
+		[NUMERO],
+		[NOMEMATERIA], 
+		[DISCIPLINA],
+		[SERIE]
+	FROM 
+		[TBMATERIA]
+
+--Retornando apenas um registro da tabela 
+	SELECT 
+		[NUMERO],
+		[NOMEMATERIA], 
+		[DISCIPLINA],
+		[SERIE]
+	FROM 
+		[TBMATERIA]
+	WHERE 
+		[NUMERO] = 4
+
+select count(*) from TBMateria
